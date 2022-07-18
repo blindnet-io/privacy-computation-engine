b01a93e274842d2bd67a4eca82c8490648c8a2ba
m4rk055 <marko@blindnet.io>
2022-07-18 11:23:25 +0200

    Add db transactor

---
+++ src/main/scala/db/DbTransactor.scala
@@ -0,0 +1,25 @@
+package io.blindnet.privacy
+package db
+
+import cats.effect.*
+import doobie.hikari.HikariTransactor
+import doobie.util.ExecutionContexts
+
+import db.DbConfig
+
+object DbTransactor {
+
+  def make(conf: DbConfig): Resource[IO, HikariTransactor[IO]] =
+    for {
+      ec <- ExecutionContexts.fixedThreadPool[IO](32)
+      xa <- HikariTransactor
+        .newHikariTransactor[IO](
+          "org.postgresql.Driver",
+          conf.uri,
+          conf.username,
+          conf.password,
+          ec
+        )
+    } yield xa
+
+}
