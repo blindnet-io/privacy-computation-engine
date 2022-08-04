75dd9b2f8242a592864e32b988058e67b5b456cc
m4rk055 <marko@blindnet.io>
2022-08-04 19:48:52 +0200

    Add eql to scope triple object

--- src/main/scala/model/vocabulary/PrivacyScope.scala
+++ src/main/scala/model/vocabulary/PrivacyScope.scala
@@ -2,9 +2,24 @@ package io.blindnet.privacy
 package model.vocabulary
 
 import model.vocabulary.terms.*
+import cats.kernel.Eq
 
 case class PrivacyScopeTriple(
     dataCategories: DataCategory,
     processingCategories: ProcessingCategory,
     purpose: Purpose
-)
+) {
+  def eql(other: PrivacyScopeTriple) = {
+    dataCategories == other.dataCategories &&
+    processingCategories == other.processingCategories &&
+    purpose == other.purpose
+  }
+
+}
+
+object PrivacyScopeTriple {
+  def unsafe(dc: String, pc: String, pp: String) =
+    PrivacyScopeTriple(DataCategory(dc), ProcessingCategory(pc), Purpose(pp))
+
+  given Eq[PrivacyScopeTriple] = Eq.instance((a, b) => a eql b)
+}
