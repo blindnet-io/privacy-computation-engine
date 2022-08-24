9b49e73dc1dba8515a3ea259b6f9394d21cc93b1
m4rk055 <marko@blindnet.io>
2022-08-24 11:58:31 +0200

    Add demand restrictions validation

--- modules/priv/src/main/scala/privacyrequest/Demand.scala
+++ modules/priv/src/main/scala/privacyrequest/Demand.scala
@@ -39,11 +39,27 @@ case class Demand(
     case _                             => None
   }.headOption
 
+  def hasValidRestrictions = {
+    lazy val hasConsentR        = restrictions.exists(r => r.isInstanceOf[Restriction.Consent])
+    // Consent Restriction with any other type of Restriction
+    lazy val consentRWithOthers = hasConsentR && restrictions.length > 1
+    // Consent Restriction within a Demand other than REVOKE-CONSENT
+    lazy val consentRNoRevoke   = hasConsentR && action != Action.RevokeConsent
+    // More than one Data Reference Restriction
+    lazy val moreOneDrefR   = restrictions.count(r => r.isInstanceOf[Restriction.DataReference]) > 1
+    // More than one Date Range Restriction
+    lazy val moreOneDrangeR = restrictions.count(r => r.isInstanceOf[Restriction.DateRange]) > 1
+
+    !consentRWithOthers &&
+    !consentRNoRevoke &&
+    !moreOneDrefR &&
+    !moreOneDrangeR
+  }
+
 }
 
 object Demand {
   def validate(demand: Demand): ValidatedNel[String, Demand] = {
-    // TODO: validate restrictions
     demand.valid
   }
 
