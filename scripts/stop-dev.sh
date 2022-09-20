9c89c13b56196765f91d813e3d3622bdd54a25d3
Noel Mace <contact@noelmace.com>
2022-09-20 15:24:37 +0200

    build: remove pce docker images on stop

--- scripts/stop-dev.sh
+++ scripts/stop-dev.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 docker compose -f docker-compose.dev.yml down
+docker rmi $(docker images -f reference=pce -q --no-trunc)
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 docker compose -f docker-compose.yml -f docker-compose.app.yml down
+docker rmi $(docker images -f reference=pce -q --no-trunc)
