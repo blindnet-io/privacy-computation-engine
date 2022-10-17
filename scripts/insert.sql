8f89ff0870090d782758e47dee11e3abd55efccd
Noel Mace <contact@noelmace.com>
2022-10-17 15:54:57 +0200

    fix: default DAC url
    use locahost (cf. dac project) by default

--- scripts/insert.sql
+++ scripts/insert.sql
@@ -1,5 +1,7 @@
 insert into apps values ('6f083c15-4ada-4671-a6d1-c671bc9105dc');
 
-insert into dac values ('6f083c15-4ada-4671-a6d1-c671bc9105dc', true, 'https://test');
+-- insert into dac values ('6f083c15-4ada-4671-a6d1-c671bc9105dc', true, 'https://blindnet-dac-staging.azurewebsites.net/');
+
+insert into dac values ('6f083c15-4ada-4671-a6d1-c671bc9105dc', true, 'http://localhost:8028/');
 
 insert into automatic_responses_config values ('6f083c15-4ada-4671-a6d1-c671bc9105dc', true, true, true, false);
