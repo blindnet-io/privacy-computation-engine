747082a8a445eaf5a98d591b51fcdec1fc9027ad
m4rk055 <marko@blindnet.io>
2022-10-26 15:03:39 +0200

    Timeline event casting

--- modules/priv/src/main/scala/TimelineEvent.scala
+++ modules/priv/src/main/scala/TimelineEvent.scala
@@ -3,6 +3,7 @@ package priv
 
 import java.time.Instant
 import java.util.UUID
+import scala.reflect.TypeTest
 
 import terms.*
 
@@ -37,12 +38,24 @@ enum TimelineEvent(timestamp: Instant) {
       scope: PrivacyScope
   ) extends TimelineEvent(timestamp)
 
+  def asLegalBase      = as[LegalBase]
+  def asConsentGiven   = as[ConsentGiven]
+  def asConsentRevoked = as[ConsentRevoked]
+  def asRestrict       = as[Restrict]
+  def asObject         = as[Object]
+
+  def as[T <: TimelineEvent](using TypeTest[TimelineEvent, T]): Option[T] =
+    this match
+      case t: T => Some(t)
+      case _    => None
+
   val getTimestamp = this.timestamp
 
   def getLbId = this match {
-    case lb: LegalBase    => Some(lb.lbId)
-    case cg: ConsentGiven => Some(cg.lbId)
-    case _                => None
+    case lb: LegalBase      => Some(lb.lbId)
+    case cg: ConsentGiven   => Some(cg.lbId)
+    case cr: ConsentRevoked => Some(cr.lbId)
+    case _                  => None
   }
 
   val getScope = this match {
