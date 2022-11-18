22f708fb5091f5a4c877b9586c808d397da76875
m4rk055 <marko@blindnet.io>
2022-11-18 11:39:48 +0100

    Fix migration

--- modules/core/src/main/resources/db/migration/V0016__New_automatic_responses_config_options.sql
+++ modules/core/src/main/resources/db/migration/V0016__New_automatic_responses_config_options.sql
@@ -2,5 +2,5 @@ alter table automatic_responses_config
   rename column auto_consents to auto_revoke_consent;
 
 alter table automatic_responses_config
-  add auto_restrict boolean not null,
-  add auto_object boolean not null;
+  add auto_restrict boolean not null default true,
+  add auto_object boolean not null default true;
